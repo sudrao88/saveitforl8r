a79d35bb9a21c505a4bd42e4e23052ddcdab2d34
m.sudhanvarao <m.sudhanvarao@gmail.com>
2026-02-08 07:09:58 +0000

    Bump cache version to v35

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // public/sw.js
-const CACHE_NAME = 'saveitforl8r-v33'; // Increment version to force update
+const CACHE_NAME = 'saveitforl8r-v35'; // Increment version to force update
 const STATIC_CACHE = 'saveitforl8r-static-v1';
 const SCOPE = '/';
 
